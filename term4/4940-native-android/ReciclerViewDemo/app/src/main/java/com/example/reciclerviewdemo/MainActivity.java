c6c63158db4e82a9744834aebfd19ad801d71adc
Maria Beatriz Camargo <mbeatrizcamargo@hotmail.com>
2024-07-09 17:42:26 -0700

    docs: 4940 classwork 12 complete

--- term4/4940-native-android/ReciclerViewDemo/app/src/main/java/com/example/reciclerviewdemo/MainActivity.java
+++ term4/4940-native-android/ReciclerViewDemo/app/src/main/java/com/example/reciclerviewdemo/MainActivity.java
@@ -7,8 +7,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+    RecyclerView.Adapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,11 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        recyclerView = findViewById(R.id.recycler_view);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        adapter = new RecyclerAdapter();
+        recyclerView.setAdapter(adapter);
     }
 }
